647ef831d31437f5ba8aae5b7f77c0574bce6eb5
jessebett <jessebett@gmail.com>
2019-05-28 13:43:28 -0400

    Spring cleaning

--- jaxde/ode_jvp.py
+++ jaxde/ode_jvp.py
@@ -1,4 +1,4 @@
-from jax import vjp,jvp
+from jax import vjp, jvp
 from jax.flatten_util import ravel_pytree
 import jax.numpy as np
 from jax import custom_transforms, ad
@@ -6,71 +6,51 @@ from jax import custom_transforms, ad
 from jaxde.odeint import odeint
 
 
-# def jvp_odeint_z_vjp(tangent_z, func, y0, t0, t1): # legacy
-#
-#     init_state, unpack = ravel_pytree((y0, tangent_z))  # get an un-concatenate function
-#
-#     def augmented_dynamics(augmented_state, t):
-#         y, adj_y = unpack(augmented_state) # augmented_state given in ode_w_linear_part
-#         dy_dt, vjp_all = vjp(func, y, t) # dy_dt= f(y,t), vjp_all = (df/dy, df/dt)
-#         vjp_a, vjp_t = vjp_all(adj_y) 
-#         return np.concatenate([dy_dt, vjp_a])
-#
-#     yt, jvp_y = unpack(odeint(augmented_dynamics, init_state, np.array([t0, t1]))[1])
-#     return yt, jvp_y
+def jvp_odeint(func, (y0, t0, t1, fargs), (tan_y0, tan_t0, tan_t1, tan_fargs)):
 
-def jvp_odeint(func, (y0, t0, t1, fargs),(t_y0,t_t0,t_t1,t_fargs)):
+    # get an un-concatenate function
+    init_state, unpack = ravel_pytree((y0, tan_y0, np.zeros_like(y0)))
 
-   init_state, unpack = ravel_pytree((y0, t_y0, np.zeros_like(y0)))  # get an un-concatenate function
-   # init_state, unpack = ravel_pytree((y0, t_y0, -t_t0*func(y0,t0,*fargs)))  # get an un-concatenate function
+    def augmented_dynamics(augmented_state, t):
 
-   def augmented_dynamics(augmented_state, t):
+        # adj_theta for both t0 and args
+        y, adj_y, adj_theta = unpack(augmented_state)
 
-       y, adj_y, adj_theta = unpack(augmented_state)
+        # dynamics for adj_y state
+        func_yt = lambda y: func(y, t, *fargs)
+        dy_dt, jvp_a = jvp(func_yt, (y, ), (adj_y, ))
 
-       func_yt = lambda y: func(y,t,*fargs)
-       dy_dt, jvp_a = jvp(func_yt, (y,),(adj_y,))
+        # dynamics accumulating t0 and fargs
+        func_theta = lambda t, fargs: func(y, t, *fargs)  #rename
+        _, jvp_theta = jvp(func_theta, (t, fargs), (tan_t0, tan_fargs))
 
-       func_theta = lambda t, fargs: func(y,t,*fargs) #rename
-       _, jvp_theta = jvp(func_theta, (t,fargs), (t_t0,t_fargs))
+        # pack back to give dynamics of adjoint state
+        return np.concatenate([dy_dt, jvp_a, jvp_theta])
 
-       return np.concatenate([dy_dt, jvp_a, jvp_theta])
+    # Solve augmented dynamics and return solution at t1
+    yt, jvp_y, jvp_theta = unpack(
+        odeint(augmented_dynamics, init_state, np.array([t0, t1]))[1])
 
-   yt, jvp_y, jvp_theta = unpack(odeint(augmented_dynamics, init_state, np.array([t0, t1]))[1])
-   jvp_t1 = t_t1*func(yt,t1,*fargs)
-   jvp_t0 = t_t0*func(yt,t1,*fargs)
+    # Sensitivities of y(t1) wrt t0 and t1
+    jvp_t0 = -tan_t0 * func(yt, t1, *fargs)
+    jvp_t1 = tan_t1 * func(yt, t1, *fargs)
 
-   return (yt, jvp_y+jvp_t1+jvp_theta-jvp_t0)
+    # Combine sensitivities
+    return (yt, jvp_y + jvp_t1 + jvp_theta + jvp_t0)
 
 
-# def jvp_odeint_all(tangent_all, func, y0, t0, t1, func_args):  # future version will take args, and [t]
-#     flat_args, unravel_args = ravel_pytree(func_args)
-#     _, unpack = ravel_pytree((y0, y0, flat_args))  # get an un-concatenate function
-#
-#
-#     def flat_func(y, t, flat_args):
-#         return func(y, t, *unravel_args(flat_args))
-#
-#     def augmented_dynamics(augmented_state, t, flat_args):
-#         y, adj_y, adj_args = unpack(augmented_state)
-#         dy_dt, vjp_all = vjp(flat_func, y, t, flat_args)
-#         vjp_a, vjp_t, vjp_args = vjp_all(adj_y)
-#         return np.concatenate([dy_dt, vjp_a, vjp_args])
-#
-#     yt, jvp_y, jvp_args = ode_w_linear_part(augmented_dynamics, y0, tangent_all, t0, t1, (flat_args,))
-#     return yt, jvp_y
-# ad.primitive_jvps[odeint] = jvp_odeint_all
-
-
-#@custom_transforms
+#@custom_transforms #TODO: remove this?
 def ode_w_linear_part(func, y0, a0, t0, t1, func_args):
-    # Just a wrapper around odeint for dynamics that are linear in a0, but not in y0.
+    """ Wrapper around odeint for dynamics that are linear in a0 but not y0"""
     aug_y0, unpack = ravel_pytree((y0, a0))
     aug_ans = odeint(func, aug_y0, np.array([t0, t1]), func_args)
     yt, jvp_all = unpack(aug_ans[1])
     return yt, jvp_all
 
+
 def odeint_w_linear_part_transpose(cotangent_y0, yt, func, y0, a0, t0, t1):
     assert a0 is None and y0 is not None  # linear in a0 only.
-    return ode_w_linear_part(func, yt, cotangent_y0, t1, t0)  # Run ODE backward.
-#ad.primitive_transposes[ode_w_linear_part] = odeint_w_linear_part_transpose
+    return ode_w_linear_part(func, yt, cotangent_y0, t1,
+                             t0)  # Run ODE backward.
+
+    #ad.primitive_transposes[ode_w_linear_part] = odeint_w_linear_part_transpose #TODO: remove this too?
--- tests/test_jvp.py
+++ tests/test_jvp.py
@@ -2,9 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-
 import jax.numpy as np
-from  numpy.random import randn
+from numpy.random import randn
 from jax.config import config
 config.update("jax_enable_x64", True)
 
@@ -14,74 +13,86 @@ from jax import custom_transforms, ad
 from jaxde.odeint import odeint
 from jaxde.ode_jvp import jvp_odeint
 
-
+# Parameters for the test function
+#TODO: Test more functions
 D = 4
 t0 = 0.1
 t1 = 0.11
 y0 = np.linspace(0.1, 0.9, D)
 fargs = (0.1, 0.2)
+
+
 def f(y, t, arg1, arg2):
     return -np.sqrt(t) - np.sin(np.dot(y, arg1)) - np.mean((y + arg2)**2)
 
+
 def test_odeint_jvp_z():
-
     def odeint2(y0):
-        return odeint(f, y0, np.array([t0, t1]), fargs, atol=1e-8, rtol=1e-8)[1]
+        return odeint(f, y0, np.array([t0, t1]), fargs, atol=1e-8,
+                      rtol=1e-8)[1]
 
-    def odeint2_jvp((y0,), (tangent_z,)):
-        tangent_t0 = 0.
-        tangent_t1 = 0.
-        tangent_fargs = (0.,0.)
-        return jvp_odeint(f,(y0,t0,t1,fargs), (tangent_z,tangent_t0,tangent_t1,tangent_fargs))
+    def odeint2_jvp((y0, ), (tan_y, )):
+        tan_t0 = 0.
+        tan_t1 = 0.
+        tan_fargs = (0., 0.)
+        return jvp_odeint(f, (y0, t0, t1, fargs),
+                          (tan_y, tan_t0, tan_t1, tan_fargs))
 
-    check_jvp(odeint2, odeint2_jvp, (y0,))
+    check_jvp(odeint2, odeint2_jvp, (y0, ))
+
 
 def test_odeint_jvp_t0():
-
     def odeint2(t0):
-        return odeint(f, y0, np.array([t0, t1]), fargs, atol=1e-8, rtol=1e-8)[1]
+        return odeint(f, y0, np.array([t0, t1]), fargs, atol=1e-8,
+                      rtol=1e-8)[1]
 
-    def odeint2_jvp((t0,), (tangent_t0,)):
-        tangent_z = np.zeros_like(y0)
-        tangent_fargs = (0.,0.)
-        tangent_t1 = 0.
-        return jvp_odeint(f,(y0,t0,t1,fargs), (tangent_z,tangent_t0,tangent_t1,tangent_fargs))
+    def odeint2_jvp((t0, ), (tan_t0, )):
+        tan_y = np.zeros_like(y0)
+        tan_fargs = (0., 0.)
+        tan_t1 = 0.
+        return jvp_odeint(f, (y0, t0, t1, fargs),
+                          (tan_y, tan_t0, tan_t1, tan_fargs))
 
-    check_jvp(odeint2, odeint2_jvp, (t0,))
+    check_jvp(odeint2, odeint2_jvp, (t0, ))
+
 
 def test_odeint_jvp_t1():
-
     def odeint2(t1):
-        return odeint(f, y0, np.array([t0, t1]), fargs, atol=1e-8, rtol=1e-8)[1]
+        return odeint(f, y0, np.array([t0, t1]), fargs, atol=1e-8,
+                      rtol=1e-8)[1]
 
-    def odeint2_jvp((t1,), (tangent_t1,)):
-        tangent_z = np.zeros_like(y0)
-        tangent_fargs = (0.,0.)
-        tangent_t0 = 0.
-        return jvp_odeint(f,(y0,t0,t1,fargs), (tangent_z,tangent_t0,tangent_t1,tangent_fargs)) #t1 is dummy
+    def odeint2_jvp((t1, ), (tan_t1, )):
+        tan_y = np.zeros_like(y0)
+        tan_fargs = (0., 0.)
+        tan_t0 = 0.
+        return jvp_odeint(f, (y0, t0, t1, fargs),
+                          (tan_y, tan_t0, tan_t1, tan_fargs))  #t1 is dummy
 
-    check_jvp(odeint2, odeint2_jvp, (t1,))
+    check_jvp(odeint2, odeint2_jvp, (t1, ))
+
 
 def test_odeint_jvp_fargs():
-
     def odeint2(fargs):
-        return odeint(f, y0, np.array([t0, t1]), fargs, atol=1e-8, rtol=1e-8)[1]
+        return odeint(f, y0, np.array([t0, t1]), fargs, atol=1e-8,
+                      rtol=1e-8)[1]
 
-    def odeint2_jvp((fargs,), (tangent_fargs,)):
-        tangent_z = np.zeros_like(y0)
-        tangent_t1 = 0. 
-        tangent_t0 = 0. 
-        return jvp_odeint(f,(y0,t0,t1,fargs), (tangent_z,tangent_t0,tangent_t1,tangent_fargs)) 
+    def odeint2_jvp((fargs, ), (tan_fargs, )):
+        tan_y = np.zeros_like(y0)
+        tan_t1 = 0.
+        tan_t0 = 0.
+        return jvp_odeint(f, (y0, t0, t1, fargs),
+                          (tan_y, tan_t0, tan_t1, tan_fargs))
 
-    check_jvp(odeint2, odeint2_jvp, (fargs,))
+    check_jvp(odeint2, odeint2_jvp, (fargs, ))
+
 
 def test_odeint_jvp():
+    def odeint2(y0, t0, t1, fargs):
+        return odeint(f, y0, np.array([t0, t1]), fargs, atol=1e-12,
+                      rtol=1e-12)[1]
 
-    def odeint2(y0,t0,t1,fargs):
-        return odeint(f, y0, np.array([t0, t1]), fargs, atol=1e-12, rtol=1e-12)[1]
+    def odeint2_jvp((y0, t0, t1, fargs), (tan_y, tan_t0, tan_t1, tan_fargs)):
+        return jvp_odeint(f, (y0, t0, t1, fargs),
+                          (tan_y, tan_t0, tan_t1, tan_fargs))  #t0 is dummy
 
-    def odeint2_jvp((y0,t0,t1,fargs), (tangent_z,tangent_t0,tangent_t1,tangent_fargs)):
-        return jvp_odeint(f,(y0,t0,t1,fargs), (tangent_z,tangent_t0,tangent_t1,tangent_fargs)) #t0 is dummy
-
-    check_jvp(odeint2, odeint2_jvp, (y0,t0,t1,fargs))
-
+    check_jvp(odeint2, odeint2_jvp, (y0, t0, t1, fargs))
